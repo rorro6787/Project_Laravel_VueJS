b6882b29316798532b8acd80b2b99b2b5b515d2a
rorro6787 <emiliorodrigo.ecr@gmail.com>
2024-08-12 15:46:40 +0200

    First miny app prepared

---
+++ src/app_basica/resources/views/resumes/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    OLA WNAS DESDE MIS HUEVOPS -> {{ $data }}
+</div>
+@endsection
---
+++ src/app_basica/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ResumeController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/resumes/create', [ResumeController::class, 'create']);
